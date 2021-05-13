43ca3e77325292665c80ec8b7cd7d2965d6c13ce
Jason McElvain <jasonom2@yahoo.com>
2021-05-13 09:35:12 -0500

    additonal folders

--- js/nbaLogic.js
+++ js/nbaLogic.js
@@ -1,4 +1,4 @@
-fetch("https://api-basketball.p.rapidapi.com/standings/groups?season=2019-2020&league=3", {
+fetch("https://api-basketball.p.rapidapi.com/games?season=2020&league=1&date=2019-11-26&team=1", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "eb2f99bc89msh5ddb3ef01b4cb06p19f493jsn25b29d062ef5",
