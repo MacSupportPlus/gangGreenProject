2450445adca8524e036161cd34013e11fc2ed6c1
Jason McElvain <jasonom2@yahoo.com>
2021-05-18 11:01:14 -0500

    added js

--- js/nbaLogic.js
+++ js/nbaLogic.js
@@ -153,8 +153,8 @@ xhr.addEventListener("readystatechange", function () {
 		let playerObj = (JSON.parse(this.responseText));
 		let index = 0
 		let playerList = []
-		let allStarsWest = [135, 381, 1267, 524, 264, 125, 547, 123, 313, 188, 143, 431, 382, 278, 318, 346, 414, 63, 191, 839]
-		let allStarsEast = [1486, 226, 462, 316, 543, 44, 537, 881, 85, 723, 19, 360, 440, 455, 760, 1045, 152, 260, 158, 480]
+		//let allStarsWest = [135, 381, 1267, 524, 264, 125, 547, 123, 313, 188, 143, 431, 382, 278, 318, 346, 414, 63, 191, 839]
+		//let allStarsEast = [1486, 226, 462, 316, 543, 44, 537, 881, 85, 723, 19, 360, 440, 455, 760, 1045, 152, 260, 158, 480]
 		for (index = 0; index < playerObj.api.players.length; index++) {
 			playerList.push(playerObj.api.players[index])
 		}
